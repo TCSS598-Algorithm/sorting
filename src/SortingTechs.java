e91085198b13038f9c94a0ebddff4775aa2f286c
Arshdeep Singh <Asigi@users.noreply.github.com>
2017-10-23 22:19:27 -0700

    added in second sorting algorithm. Removed tacoma/seattle sorting and added washington sorting.

--- src/SortingTechs.java
+++ src/SortingTechs.java
@@ -18,28 +18,21 @@ public class SortingTechs {
 		//Year,WEEK,Week Ending Date,REGION,State,City,Pneumonia and Influenza Deaths,All Deaths,<1 year (all cause deaths),1-24 years (all cause deaths),25-44 years,45-64 years (all cause deaths),65+ years (all cause deaths)
 		//0    1    2                3      4     5    6                              7          8                          9                             10                                         11
 		
-		String csvFile = "src/WashDeath.csv";
-		String csvFile2 = "src/TacomaDeaths.csv";
+
 		String csvFile3 = "src/WashDeath.csv";
 		
 		//TODO read data sets 1,2,3,4 into 4 different array objects 
-		//int[] bostonAllDeathNumbers = new int[522]; //522 weeks of data.
-		//int[] tacomaFluDeathNumbers = new int[522]; //522 weeks of data.
+
 		int[] washingtonDeathNumbers = new int[8558];
 		
 		//read data into array
-		//readIn(csvFile, bostonAllDeathNumbers);
-		//readIn(csvFile2, tacomaFluDeathNumbers);
 		readIn(csvFile3, washingtonDeathNumbers);
 		
-		//System.out.println("last" + bostonAllDeathNumbers[104]); //test to make sure the last value is 161
-		//System.out.println("last" + tacomaFluDeathNumbers[104]); //test to make sure the last value is 11
-		
-		//TODO call each of the 4 sorting method 4 times and pass in a copy of one of the array objects each time
-		//firstSortingMethod(bostonAllDeathNumbers, 522);
-		//firstSortingMethod(tacomaFluDeathNumbers, 522);
-		firstSortingMethod(washingtonDeathNumbers, 8558);
 
+		//TODO call each of the 4 sorting method 4 times and pass in a copy of one of the array objects each time
+		firstSortingMethod(washingtonDeathNumbers.clone(), 8558);
+		secondSortingMethod(washingtonDeathNumbers.clone(), 8558);
+		System.out.println();
 		
 	
 	}
@@ -101,7 +94,7 @@ public class SortingTechs {
 	//end timer. save that time into a file.
 	private static void firstSortingMethod(int[] theArray, int theArraySize) {
 		System.out.println("------Insertion Sort----------");
-		System.out.println("---------------------sorting:   " + arrayPrinter(theArray));
+		//System.out.println("---------------------sorting:   " + arrayPrinter(theArray));
 		
 		long startTimer = System.currentTimeMillis(); //start timer
 		
@@ -124,7 +117,7 @@ public class SortingTechs {
 		long finalTime = endTimer - startTimer;
 		
 		System.out.println("It took " + finalTime + " ms to finish first sorting algorithm.");
-		System.out.println("---------------------sorted :   " + arrayPrinter(theArray));
+		//System.out.println("---------------------sorted :   " + arrayPrinter(theArray));
 		System.out.println("-------------------------------------------");
 	}
 	
@@ -136,6 +129,31 @@ public class SortingTechs {
 	//start timer
 	//sort the first array
 	//end timer. save that time into a file.
+	private static void secondSortingMethod(int[] theArray, int theArraySize) {
+		System.out.println("------Selection Sort----------");
+		//System.out.println("---------------------sorting:   " + arrayPrinter(theArray));
+		
+		long startTimer = System.currentTimeMillis(); //start timer
+		
+		for (int a = 0; a < theArraySize - 1; a++)
+        {
+            int index = a;
+            for (int b = a + 1; b < theArraySize; b++)
+                if (theArray[b] < theArray[index]) 
+                    index = b;
+      
+            int smaller = theArray[index];  
+            theArray[index] = theArray[a];
+            theArray[a] = smaller;
+        }
+		
+		long endTimer = System.currentTimeMillis(); //end timer
+		long finalTime = endTimer - startTimer;
+		
+		System.out.println("It took " + finalTime + " ms to finish first sorting algorithm.");
+		//System.out.println("---------------------sorted :   " + arrayPrinter(theArray));
+		System.out.println("-------------------------------------------");
+	}
 	
 	
 	//THIRD METHOD OF SORTING
