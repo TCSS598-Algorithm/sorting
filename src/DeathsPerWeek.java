262d1d1f9facf66041ff1cca09c41ff45597c44c
Todd Robbins <toddrobb11@gmail.com>
2017-10-24 21:45:54 -0700

    modelObject
    flavor flav

--- src/DeathsPerWeek.java
+++ src/DeathsPerWeek.java
@@ -5,12 +5,20 @@
  * 
  */
 public class DeathsPerWeek {
+	private int year, week, diseaseDeath, DeathTotal, deathBaby,
+	deathYouth, deathMiddle, deathOld;
+	private String weekDate, region, state, city; 
 
 	/**
 	 * 
 	 */
 	public DeathsPerWeek() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public DeathsPerWeek(int year, int week, String weekDate, String region,
+			String state, String city, int diseaseDeath, int DeathTotal,
+			int deathBaby, int deathYouth, int deathMiddle, int deathOld) {
 	}
 
 }
