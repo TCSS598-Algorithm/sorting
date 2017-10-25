b939b3b5573bc4201f02818738abb71453ad9c74
Todd Robbins <toddrobb11@gmail.com>
2017-10-24 21:21:42 -0700

    model object for deathsperweek csv(s)
    wakka wakka

---
+++ src/DeathsPerWeek.java
@@ -0,0 +1,16 @@
+/**
+ * @author Arshdeep Singh
+ * @author Todd Robbins
+ * model object for csv rows. 
+ * 
+ */
+public class DeathsPerWeek {
+
+	/**
+	 * 
+	 */
+	public DeathsPerWeek() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
